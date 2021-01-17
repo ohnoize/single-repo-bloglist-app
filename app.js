73a3cd7f2aa54c3fcfed423b1ccddd4f155833e4
Olli Hirvonen <ollihirvonen@Ollis-MBP.fios-router.home>
2021-01-17 13:00:58 -0500

    starting to configure initial structure

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('server started on port 5000')
+})
+
+
